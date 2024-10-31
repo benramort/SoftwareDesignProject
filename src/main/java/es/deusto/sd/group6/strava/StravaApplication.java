9ca24de3ef217e055943e0952a8e4246bf387149
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-10-31 16:28:51 +0100

    Initial commit

---
+++ src/main/java/es/deusto/sd/group6/strava/StravaApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.group6.strava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaApplication.class, args);
+	}
+
+}
