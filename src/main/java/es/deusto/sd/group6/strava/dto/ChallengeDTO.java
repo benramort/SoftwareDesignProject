451890b08b1a79684fc301775b2813c8a635d58b
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-11-05 12:04:10 +0100

    Squashed commit of the following:
    commit 2ec0429ffbfd507b1482d72d7c32a4fa013d072e
    Author: Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
    Date:   Tue Nov 5 12:02:41 2024 +0100
    
        Accept challenge fixed
    
        Co-Authored-By: Joana Artetxe Goti <145749032+jArtetxe@users.noreply.github.com>
    
    commit 1ad959b173ce3fcadbdf2230582c8125de2b717f
    Author: jArtetxe <j.artetxe@opendeusto.es>
    Date:   Mon Nov 4 21:56:46 2024 +0100
    
        challengeFix
    
    commit 4c19ec4b8d2cfebabb86c467e41894b88697b507
    Author: jArtetxe <j.artetxe@opendeusto.es>
    Date:   Mon Nov 4 21:21:54 2024 +0100
    
        challenges

---
+++ src/main/java/es/deusto/sd/group6/strava/dto/ChallengeDTO.java
@@ -0,0 +1,76 @@
+package es.deusto.sd.group6.strava.dto;
+
+import java.util.Date;
+
+import es.deusto.sd.group6.strava.entity.Sport;
+
+public class ChallengeDTO {
+	
+	private String name;
+    private Date startDate;
+    private Date endDate;
+    private boolean isDistance;
+    private Sport sport;
+    private float goal;
+	
+	public ChallengeDTO() {}	
+	
+	public ChallengeDTO(String name, Date startDate, Date endDate, boolean isDistance, float goal, Sport sport) {
+		super();
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.isDistance = isDistance;
+		this.goal = goal;
+		this.sport = sport;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public boolean isDistance() {
+		return isDistance;
+	}
+
+	public void setDistance(boolean isDistance) {
+		this.isDistance = isDistance;
+	}
+
+	public float getGoal() {
+		return goal;
+	}
+
+	public void setGoal(float goal) {
+		this.goal = goal;
+	}
+
+	public Sport getSport() {
+		return sport;
+	}
+
+	public void setSport(Sport sport) {
+		this.sport = sport;
+	}
+	
+}
