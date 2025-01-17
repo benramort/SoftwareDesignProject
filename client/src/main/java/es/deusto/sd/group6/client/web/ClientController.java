0ef231236d6295932a452a4e270308de3ff768d3
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2025-01-17 18:57:54 +0100

    Estilo

--- client/src/main/java/es/deusto/sd/group6/client/web/ClientController.java
+++ client/src/main/java/es/deusto/sd/group6/client/web/ClientController.java
@@ -1,5 +1,6 @@
 package es.deusto.sd.group6.client.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClientController {
+	
+	@Autowired
+	IStravaServiceProxy stravaService;
 	
 	@GetMapping("/")
 	public String home(Model model) {
--- client/src/main/java/es/deusto/sd/group6/client/web/ServiceProxy.java
+++ client/src/main/java/es/deusto/sd/group6/client/web/ServiceProxy.java
@@ -1,12 +1,23 @@
 package es.deusto.sd.group6.client.web;
 
+import java.net.http.HttpClient;
+
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
-public class ServiceProxy {
+public class ServiceProxy implements IStravaServiceProxy {
+	
+	private static final String BASE_URL = "http://localhost:8080";
+	
+	private final ObjectMapper objectMapper;
+	private final HttpClient httpClient;
+	
 	
 	public ServiceProxy() {
-		
+		objectMapper = new ObjectMapper();
+        httpClient = HttpClient.newHttpClient();
 	}
 
 }
